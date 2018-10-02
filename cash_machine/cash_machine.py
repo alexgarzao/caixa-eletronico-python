161fdffc0ce2569c9572831b5ee9273154e10c7b
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 20:45:45 -0300

    REFACTOR: Movendo codigo para metodos auxiliares

--- cash_machine/cash_machine.py
+++ cash_machine/cash_machine.py
@@ -3,15 +3,34 @@ class CashMachine:
         self.__available_cash_notes = [100, 50, 20, 10]
 
     def cash_notes_for_withdrawal(self, value):
-        cash_notes = {note: 0 for note in self.__available_cash_notes}
+        self.cash_note_idx = 0
+        cash_notes = self.__initial_cash_notes()
+        cash_note = self.__get_next_cash_note()
 
-        cash_note_idx = 0
         while value > 0:
-            cash_note = self.__available_cash_notes[cash_note_idx]
-            if cash_note > value:
-                cash_note_idx += 1
-                continue
-            cash_notes[cash_note] += 1
-            value -= cash_note
+            while value >= cash_note:
+                cash_notes[cash_note] += 1
+                value -= cash_note
 
-        return cash_notes
+            if value == 0:
+                return cash_notes
+
+            cash_note = self.__get_next_cash_note()
+            if cash_note == 0:
+                return self.__initial_cash_notes()
+
+        return self.__initial_cash_notes()
+
+    def __initial_cash_notes(self):
+        return {note: 0 for note in self.__available_cash_notes}
+
+    def __get_next_cash_note(self):
+        if self.__no_more_notes():
+            return 0
+
+        cash_note = self.__available_cash_notes[self.cash_note_idx]
+        self.cash_note_idx += 1
+        return cash_note
+
+    def __no_more_notes(self):
+        return self.cash_note_idx == len(self.__available_cash_notes)
