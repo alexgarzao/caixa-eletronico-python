3cc61902e962ddf01ca581863c4faa48462d993d
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 20:13:00 -0300

    Novos cenarios validos de teste e alguns invalidos.
    Cenarios validos com numeros maiores. Cenarios invalidos como 0,
    negativo, ...

--- tests/cash_machine_test.py
+++ tests/cash_machine_test.py
@@ -11,6 +11,41 @@ def check_withdrawal_50_test():
     __assert_result(cm.cash_notes_for_withdrawal(50), {100: 0, 50: 1, 20: 0, 10: 0})
 
 
+def check_withdrawal_200_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(200), {100: 2, 50: 0, 20: 0, 10: 0})
+
+
+def check_withdrawal_150_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(150), {100: 1, 50: 1, 20: 0, 10: 0})
+
+
+def check_withdrawal_80_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(80), {100: 0, 50: 1, 20: 1, 10: 1})
+
+
+def check_withdrawal_30_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(30), {100: 0, 50: 0, 20: 1, 10: 1})
+
+
+def check_withdrawal_0_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(0), {100: 0, 50: 0, 20: 0, 10: 0})
+
+
+def check_withdrawal_negative_number_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(-20), {100: 0, 50: 0, 20: 0, 10: 0})
+
+
+def check_withdrawal_2540_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(2540), {100: 25, 50: 0, 20: 2, 10: 0})
+
+
 def __assert_result(result, expected):
     assert result == expected, \
         "Resultado={} Esperado={}".format(result, expected)
