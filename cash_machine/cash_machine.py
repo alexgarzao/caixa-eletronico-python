1fb95db6d3c9d85b77af385fca96202b4edb68ab
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 20:03:55 -0300

    Teste validando saque de 100 e 50. Primeira implementacao real do saque.

--- cash_machine/cash_machine.py
+++ cash_machine/cash_machine.py
@@ -3,4 +3,15 @@ class CashMachine:
         self.__available_cash_notes = [100, 50, 20, 10]
 
     def cash_notes_for_withdrawal(self, value):
-        return {"100": 1}
+        cash_notes = {note: 0 for note in self.__available_cash_notes}
+
+        cash_note_idx = 0
+        while value > 0:
+            cash_note = self.__available_cash_notes[cash_note_idx]
+            if cash_note > value:
+                cash_note_idx += 1
+                continue
+            cash_notes[cash_note] += 1
+            value -= cash_note
+
+        return cash_notes
--- tests/cash_machine_test.py
+++ tests/cash_machine_test.py
@@ -3,4 +3,14 @@ from cash_machine.cash_machine import CashMachine
 
 def check_withdrawal_100_test():
     cm = CashMachine()
-    assert cm.cash_notes_for_withdrawal(100) == {"100": 1}
+    __assert_result(cm.cash_notes_for_withdrawal(100), {100: 1, 50: 0, 20: 0, 10: 0})
+
+
+def check_withdrawal_50_test():
+    cm = CashMachine()
+    __assert_result(cm.cash_notes_for_withdrawal(50), {100: 0, 50: 1, 20: 0, 10: 0})
+
+
+def __assert_result(result, expected):
+    assert result == expected, \
+        "Resultado={} Esperado={}".format(result, expected)
