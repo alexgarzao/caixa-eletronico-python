febe29605c2853741ce7786a8b6db650e1918172
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 21:37:20 -0300

    Testes de mensagem com o valor 80, 30, 0, negativo, e impossivel.

--- tests/cash_machine_display_test.py
+++ tests/cash_machine_display_test.py
@@ -21,3 +21,33 @@ def check_withdrawal_200_test():
 def check_withdrawal_150_test():
     cmd = CashMachineDisplay()
     eq_(cmd.message_for_withdrawal(150), "Entregar 1 nota(s) de R$ 100,00, 1 nota(s) de R$ 50,00")
+
+
+def check_withdrawal_80_test():
+    cmd = CashMachineDisplay()
+    eq_(cmd.message_for_withdrawal(80), "Entregar 1 nota(s) de R$ 50,00, 1 nota(s) de R$ 20,00, 1 nota(s) de R$ 10,00")
+
+
+def check_withdrawal_30_test():
+    cmd = CashMachineDisplay()
+    eq_(cmd.message_for_withdrawal(30), "Entregar 1 nota(s) de R$ 20,00, 1 nota(s) de R$ 10,00")
+
+
+def check_withdrawal_0_test():
+    cmd = CashMachineDisplay()
+    eq_(cmd.message_for_withdrawal(0), "")
+
+
+def check_withdrawal_negative_number_test():
+    cmd = CashMachineDisplay()
+    eq_(cmd.message_for_withdrawal(-50), "")
+
+
+def check_withdrawal_2540_test():
+    cmd = CashMachineDisplay()
+    eq_(cmd.message_for_withdrawal(2540), "Entregar 25 nota(s) de R$ 100,00, 2 nota(s) de R$ 20,00")
+
+
+def check_withdrawal_with_impossible_value_test():
+    cmd = CashMachineDisplay()
+    eq_(cmd.message_for_withdrawal(2537), "")
