477fc9c28cdff059399c6450f119af6307cf4c74
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 21:33:09 -0300

    REFACTOR: Utilizando eq_ ao inves de um assert customizado

--- tests/cash_machine_display_test.py
+++ tests/cash_machine_display_test.py
@@ -1,26 +1,23 @@
+from nose.tools import eq_
+
 from cash_machine.cash_machine_display import CashMachineDisplay
 
 
 def check_withdrawal_100_test():
     cmd = CashMachineDisplay()
-    __assert_result(cmd.message_for_withdrawal(100), "Entregar 1 nota(s) de R$ 100,00")
+    eq_(cmd.message_for_withdrawal(100), "Entregar 1 nota(s) de R$ 100,00")
 
 
 def check_withdrawal_50_test():
     cmd = CashMachineDisplay()
-    __assert_result(cmd.message_for_withdrawal(50), "Entregar 1 nota(s) de R$ 50,00")
+    eq_(cmd.message_for_withdrawal(50), "Entregar 1 nota(s) de R$ 50,00")
 
 
 def check_withdrawal_200_test():
     cmd = CashMachineDisplay()
-    __assert_result(cmd.message_for_withdrawal(200), "Entregar 2 nota(s) de R$ 100,00")
+    eq_(cmd.message_for_withdrawal(200), "Entregar 2 nota(s) de R$ 100,00")
 
 
 def check_withdrawal_150_test():
     cmd = CashMachineDisplay()
-    __assert_result(cmd.message_for_withdrawal(150), "Entregar 1 nota(s) de R$ 100,00, 1 nota(s) de R$ 50,00")
-
-
-def __assert_result(result, expected):
-    assert result == expected, \
-        "Resultado={} Esperado={}".format(result, expected)
+    eq_(cmd.message_for_withdrawal(150), "Entregar 1 nota(s) de R$ 100,00, 1 nota(s) de R$ 50,00")
