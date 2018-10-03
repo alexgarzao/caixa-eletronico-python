abbb981ffab4821bc6131494d09a499f5c3c6b18
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-03 17:30:24 -0300

    Ajustes conforme PEP8

--- tests/cash_machine_display_test.py
+++ tests/cash_machine_display_test.py
@@ -5,49 +5,79 @@ from cash_machine.cash_machine_display import CashMachineDisplay
 
 def check_withdrawal_100_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(100), "Entregar 1 nota(s) de R$ 100,00")
+    eq_(
+        cmd.message_for_withdrawal(100),
+        "Entregar 1 nota(s) de R$ 100,00"
+    )
 
 
 def check_withdrawal_50_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(50), "Entregar 1 nota(s) de R$ 50,00")
+    eq_(
+        cmd.message_for_withdrawal(50),
+        "Entregar 1 nota(s) de R$ 50,00"
+    )
 
 
 def check_withdrawal_200_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(200), "Entregar 2 nota(s) de R$ 100,00")
+    eq_(
+        cmd.message_for_withdrawal(200),
+        "Entregar 2 nota(s) de R$ 100,00"
+    )
 
 
 def check_withdrawal_150_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(150), "Entregar 1 nota(s) de R$ 100,00, 1 nota(s) de R$ 50,00")
+    eq_(
+        cmd.message_for_withdrawal(150),
+        "Entregar 1 nota(s) de R$ 100,00, 1 nota(s) de R$ 50,00"
+    )
 
 
 def check_withdrawal_80_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(80), "Entregar 1 nota(s) de R$ 50,00, 1 nota(s) de R$ 20,00, 1 nota(s) de R$ 10,00")
+    eq_(
+        cmd.message_for_withdrawal(80),
+        "Entregar 1 nota(s) de R$ 50,00, 1 nota(s) de R$ 20,00, 1 nota(s) de R$ 10,00"
+    )
 
 
 def check_withdrawal_30_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(30), "Entregar 1 nota(s) de R$ 20,00, 1 nota(s) de R$ 10,00")
+    eq_(
+        cmd.message_for_withdrawal(30),
+        "Entregar 1 nota(s) de R$ 20,00, 1 nota(s) de R$ 10,00"
+    )
 
 
 def check_withdrawal_0_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(0), "")
+    eq_(
+        cmd.message_for_withdrawal(0),
+        ""
+    )
 
 
 def check_withdrawal_negative_number_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(-50), "")
+    eq_(
+        cmd.message_for_withdrawal(-50),
+        ""
+    )
 
 
 def check_withdrawal_2540_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(2540), "Entregar 25 nota(s) de R$ 100,00, 2 nota(s) de R$ 20,00")
+    eq_(
+        cmd.message_for_withdrawal(2540),
+        "Entregar 25 nota(s) de R$ 100,00, 2 nota(s) de R$ 20,00"
+    )
 
 
 def check_withdrawal_with_impossible_value_test():
     cmd = CashMachineDisplay()
-    eq_(cmd.message_for_withdrawal(2537), "")
+    eq_(
+        cmd.message_for_withdrawal(2537),
+        ""
+    )
