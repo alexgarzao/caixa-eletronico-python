f08ddfb5da90ad5bf95bf4676d074c32091d4716
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 21:19:33 -0300

    Novos testes de mensagem: 50, 200 e 150

--- tests/cash_machine_display_test.py
+++ tests/cash_machine_display_test.py
@@ -6,6 +6,21 @@ def check_withdrawal_100_test():
     __assert_result(cmd.message_for_withdrawal(100), "Entregar 1 nota(s) de R$ 100,00")
 
 
+def check_withdrawal_50_test():
+    cmd = CashMachineDisplay()
+    __assert_result(cmd.message_for_withdrawal(50), "Entregar 1 nota(s) de R$ 50,00")
+
+
+def check_withdrawal_200_test():
+    cmd = CashMachineDisplay()
+    __assert_result(cmd.message_for_withdrawal(200), "Entregar 2 nota(s) de R$ 100,00")
+
+
+def check_withdrawal_150_test():
+    cmd = CashMachineDisplay()
+    __assert_result(cmd.message_for_withdrawal(150), "Entregar 1 nota(s) de R$ 100,00, 1 nota(s) de R$ 50,00")
+
+
 def __assert_result(result, expected):
     assert result == expected, \
         "Resultado={} Esperado={}".format(result, expected)
