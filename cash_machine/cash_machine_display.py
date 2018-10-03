f0038ed363f36b5d769c2f001b750ab61f7377a2
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-10-02 21:20:21 -0300

    Refactor: Movendo codigo para metodos

--- cash_machine/cash_machine_display.py
+++ cash_machine/cash_machine_display.py
@@ -7,11 +7,18 @@ class CashMachineDisplay:
 
     def message_for_withdrawal(self, value):
         cash_notes = self.cm.cash_notes_for_withdrawal(value)
-        message = "Entregar"
+        return self.__format_message(cash_notes)
+
+    def __format_message(self, cash_notes):
+        message = ""
+
         for cash_note, count in cash_notes.items():
             if count == 0:
                 continue
 
-            message += " {} nota(s) de R$ {},00".format(count, cash_note)
+            message += "{} nota(s) de R$ {},00, ".format(count, cash_note)
+
+        if message:
+            message = "Entregar " + message[:-2]
 
         return message
